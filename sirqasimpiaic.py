4528f499ca0b38679e0c1f6a41cf4c79a2dcfdf5
Aribamemon <aribaabdulqadir@gmail.com>
2023-10-02 21:49:10 +0500

    first commit

---
+++ sirqasimpiaic.py
@@ -0,0 +1,22 @@
+# from typing import List
+# print("Hello world")
+# # message = "hello ariba"
+# # print(message)
+
+# num1 = "abc"
+# print(type(num1))
+# print(id(num1))
+# print(num1)
+
+# num1: int = 7
+# print(type(num1))
+# print(id(num1))
+# print(num1)
+
+# num1: List[str] = [1, 2, 3]
+# print(type(num1))
+# print(id(num1))
+# print(num1)
+
+# a:str=1
+# print(a)
